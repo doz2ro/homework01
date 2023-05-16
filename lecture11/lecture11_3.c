7b59e9d3dac50b07ce91cd9c8d98524d8e7508b2
허도영 <doz2ro@heodoyeong-ui-MacBookAir.local>
2023-05-16 11:55:00 +0900

    HDY

--- lecture11/lecture11_3.c
+++ lecture11/lecture11_3.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include<string.h>
-
+int strPrint(const char* string)
+{
+    char *a;
+    a = (char* )string;
+    printf("%s",a);
+    return 0;
+}
 int main()
 {
     char a[30];
@@ -29,6 +35,8 @@ int main()
     }
 
     printf("%s",a);
+
+    strPrint("Lunch Eat!!!");
 
     return 0;
 }
